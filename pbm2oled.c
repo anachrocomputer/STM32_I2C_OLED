65e688559d1e9aaba9c5adacc7ab51b0c90c0afe
John Honniball <coredump@virginmedia.com>
2023-01-21 23:08:34 +0000

    Update to new coding standard.
    Also add comments.

--- pbm2oled.c
+++ pbm2oled.c
@@ -13,47 +13,51 @@
 // The frame buffer, 512 bytes
 unsigned char Frame[MAXROWS][MAXX];
 
-void writeOLED (const char name[]);
-int readPBM (const char name[]);
+void writeOLED(const char name[]);
+int readPBM(const char name[]);
 
-int main (int argc, char *argv[])
+int main(const int argc, const char *const argv[])
 {
-   if (readPBM (argv[1]))
-      writeOLED (argv[1]);
+   if (readPBM(argv[1]))
+      writeOLED(argv[1]);
       
    return (0);
 }
 
 
-void writeOLED (const char name[])
+/* writeOLED --- write the frame buffer into a C header file */
+
+void writeOLED(const char name[])
 {
    int x, y;
    
-   puts ("const uint8_t OLEDImage[MAXROWS * MAXX] = {");
+   puts("const uint8_t OLEDImage[MAXROWS * MAXX] = {");
 
    for (y = 0; y < MAXROWS; y++) {
       for (x = 0; x < MAXX; x++) {
          if ((x % 16) == 0)
-            printf ("  ");
+            printf("  ");
             
          printf ("0x%02x", Frame[y][x]);
          
          if ((y == MAXROWS - 1) && (x == MAXX - 1))
-            printf ("\n");
+            printf("\n");
          else if (x == 15)
-            printf (", /* row %d */\n", y);
+            printf(", /* row %d */\n", y);
          else if ((x % 16) == 15)
-            printf (",\n");
+            printf(",\n");
          else
-            printf (", ");
+            printf(", ");
       }
    }
   
-   puts ("};");
+   puts("};");
 }
 
 
-int readPBM (const char name[])
+/* readPBM --- read a binary PBM file into the frame buffer in memory */
+
+int readPBM(const char name[])
 {
    FILE *fp;
    char lin[256];
@@ -64,8 +68,8 @@ int readPBM (const char name[])
    int nb;
    unsigned char buf[MAXX];
    
-   if ((fp = fopen (name, "r")) == NULL) {
-      perror (name);
+   if ((fp = fopen(name, "r")) == NULL) {
+      perror(name);
       return (0);
    }
 
@@ -73,14 +77,14 @@ int readPBM (const char name[])
    fgets (lin, sizeof (lin), fp);
 
    if (lin[0] != 'P') {
-      fprintf (stderr, "Image file '%s' is not a PBM file\n", name);
-      fclose (fp);
+      fprintf(stderr, "Image file '%s' is not a PBM file\n", name);
+      fclose(fp);
       return (0);
    }
    
    if (lin[1] != '4') {
-      fprintf (stderr, "Image file '%s' not binary PBM file\n", name);
-      fclose (fp);
+      fprintf(stderr, "Image file '%s' not binary PBM file\n", name);
+      fclose(fp);
       return (0);
    }
 
@@ -94,8 +98,8 @@ int readPBM (const char name[])
    sscanf (lin, "%d %d", &xsize, &ysize);
    
    if (xsize > MAXX) {
-      fprintf (stderr, "Image width (%d) exceeds maximum (%d)\n", xsize, MAXX);
-      fclose (fp);
+      fprintf(stderr, "Image width (%d) exceeds maximum (%d)\n", xsize, MAXX);
+      fclose(fp);
       return (0);
    }
 
@@ -105,11 +109,11 @@ int readPBM (const char name[])
          Frame[y / 8][x] = 0;
 
       for (i = 0; i < 8; i++) {
-         if ((nb = fread (buf, sizeof (char), MAXX / 8, fp)) != (MAXX / 8)) {
+         if ((nb = fread(buf, sizeof (char), MAXX / 8, fp)) != (MAXX / 8)) {
             if ((nb == 0) && ((y + i) >= MAXY))
-               memset (buf, 0, sizeof (buf));
+               memset(buf, 0, sizeof (buf));
             else {
-               fprintf (stderr, "EOF!! (%d bytes, row %d)\n", nb, y + i);
+               fprintf(stderr, "EOF!! (%d bytes, row %d)\n", nb, y + i);
                return (0);
             }
          }
